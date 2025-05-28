07a8824c038321ce810f7b1c5e2027c223e6f74c
irufano <irfanhidayatms@gmail.com>
2025-05-28 19:15:35 +0700

    update README 0.2.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="llmfy",
-    version="0.2.4",
+    version="0.2.5",
     packages=find_packages(),
     include_package_data=True,
     description="`LLMfy` is a framework for developing applications with large language models (LLMs).",
