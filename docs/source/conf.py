ded72ae9ecee9ac30fb1defbd1ded853d35e3a43
irufano <irfanhidayatms@gmail.com>
2025-06-09 20:48:14 +0700

    update rtd conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ import sys
 from dotenv import load_dotenv
 from sphinxawesome_theme import ThemeOptions
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 load_dotenv()
 
 # Configuration file for the Sphinx documentation builder.
